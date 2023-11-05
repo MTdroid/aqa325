7e322461793dac3f227169a4bce5a448f6fb5265
”Marat <marattagirov2020@gmail.com>
2023-11-05 17:37:03 +0300

    0.6 code coverage

--- src/test/java/BoundaryServiceTest.java
+++ src/test/java/BoundaryServiceTest.java
@@ -1,2 +1,42 @@
+import academy.kata.BoundaryService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BoundaryServiceTest {
+    BoundaryService boundaryService = new BoundaryService();
+
+    @Test
+    public void findmin() {
+        int[] array = {3,2,1,6,5,1,4,4};
+        int[] array1 = {1,1};
+        int bsResult = boundaryService.findMin(array);
+        int testResult = Arrays.stream(array).min().getAsInt();
+        assertEquals(testResult,bsResult);
+    }
+
+    @Test
+    public void findminnn() {
+        int[] array = {0,1,0,0,2,2};
+        int bsResult = boundaryService.findMin(array);
+        int testResult = Arrays.stream(array).min().getAsInt();
+        assertEquals(testResult,bsResult);
+    }
 }
+
+
+
+
+/* Поиск минимального числа в массиве
+
+    public int findMin(int[] marks) {
+        int currentMin = marks[0];
+        for (int mark : marks) {
+            if (currentMin > mark) {
+                currentMin = mark;
+            }
+        }
+        return currentMin;
+    }*/

ca5924974b2fa43eb902f31fb43f96008414e4fc
”Marat <marattagirov2020@gmail.com>
2023-11-08 23:04:18 +0300

    Parameterized Test, add @DisplayName and @Description

--- src/main/java/academy/kata/Main.java
+++
@@ -1,7 +0,0 @@
-package academy.kata;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("code coverage fail build");
-    }
-}
--- src/test/java/BoundaryServiceTest.java
+++ src/test/java/BoundaryServiceTest.java
@@ -1,27 +1,31 @@
 import academy.kata.BoundaryService;
-import org.junit.jupiter.api.Test;
+import jdk.jfr.Description;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class BoundaryServiceTest {
+
     BoundaryService boundaryService = new BoundaryService();
 
-    @Test
-    public void findmin() {
-        int[] array = {3,2,1,6,5,1,4,4};
-        int bsResult = boundaryService.findMin(array);
-        int testResult = Arrays.stream(array).min().getAsInt();
-        assertEquals(testResult,bsResult);
+    @ParameterizedTest
+    @DisplayName("Find min element in array")
+    @Description("testResult must be equals bsResult")
+    @MethodSource("arrays")
+    public void findmin(int[] arr) {
+        int bsResult = boundaryService.findMin(arr);
+        int testResult = Arrays.stream(arr).min().getAsInt();
+        assertEquals(testResult, bsResult);
     }
 
-    @Test
-    public void findminnn() {
-        int[] array = {0,1,0,0,2,2};
-        int bsResult = boundaryService.findMin(array);
-        int testResult = Arrays.stream(array).min().getAsInt();
-        assertEquals(testResult,bsResult);
+    static Stream<int[]> arrays() {
+        return Stream.of(new int[]{3, 2, 1, 6, 5, 1, 4, 4}, new int[]{0, 1, 0, 0, 2, 2});
     }
 }
 
